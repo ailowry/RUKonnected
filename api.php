9b879732968cf8b9d0648b74692a37be66c4759d
Adrian Lowry <ailowry@rucs.radford.edu>
2012-04-02 10:23:20 -0400

    created file to handle api requests

---
+++ api.php
@@ -0,0 +1,80 @@
+<?php
+    require('./db.php');
+
+    if($_GET['action']) {
+        $input = $_GET;
+    }
+    else if($_POST['action']) {
+        $input = $_POST;
+    }
+
+    echo $input ? routeAction($input) : 'Error: Improper api call';
+
+    /**
+     * Takes a data set and routes it to correct operation
+     * @param $input A set of GET or POST data
+     * @return Result of routing
+     */
+    function routeAction($input) {
+        // Temporary test value
+        $userid = 1;
+
+        $action = $input['action'];
+        $err = null;
+
+        if($action == 'getMessages') {
+            $response = getMessages($userid);
+        }
+        // This will be moved into a POST request later
+        else if($action == 'makePost') {
+            if($input['friendID']) {
+                $response = makePost($userid, $input['content'], $input['friendID']);
+            }
+            else {
+                $response = makePost($userid, $input['content']);
+            }
+            echo mysql_error();
+        }
+        else {
+            $err = 'Error: Improper api call';
+        }
+
+        return $err ? $err : json_encode($response);
+    }
+
+    /**
+     * Gets an array of all messages user has sent or recieved
+     * @param $userid The id of the user requesting messages
+     * @return An array of messages the user has sent or recieved
+     */
+    function getMessages($userid) {
+        $userid = (int)$userid;
+        $qStr = "SELECT * FROM Messages WHERE (UserID = $userid "
+                . "OR FriendID = $userid)";
+        $result = mysql_query($qStr);
+        return mysql_fetch_assoc($result);
+    }
+
+    /**
+     * Makes a post to the users wall, or a friends wall
+     * @param $userid The id of the user making the post
+     * @param $content the content of the post
+     * @param $friendid The id of the friend to post to
+     * @return True if post was success, error if post failed
+     */
+    function makePost($userid, $text, $friendid = null) {
+        $userid_v = (int)$userid;
+        $text_v = mysql_real_escape_string($text);
+        if($friendid) {
+            $friendid_v = (int)$friendid;
+            $qStr = "INSERT INTO Posts(UserID, PostContent, FriendUserID, "
+                    . "Time) VALUES($userid_v, '$text_v', $friendid_v, NOW())";
+        }
+        else {
+            $qStr = "INSERT INTO Posts(UserID, PostContent, Time) "
+                    . "VALUES($userid_v, '$text_v', NOW())";
+        }
+        $result = mysql_query($qStr);
+        return mysql_fetch_assoc($result);
+    }
+?>
