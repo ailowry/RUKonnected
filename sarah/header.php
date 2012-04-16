bf6e8f6ec345184e961c50d053bc800d029a6fef
Ross Norvell <renorvell@rucs.radford.edu>
2012-04-16 13:08:55 -0400

    updated style rmed undeeded folders

--- sarah/header.php
+++
@@ -1,8 +0,0 @@
-<div id="header">
-		<img src="./imgs/RUConnected_Logo.png" alt="RUConnected Logo" />
-		<input type="button" name="friendrequests" value="Friend Requests" />
-		<input type="button" name="messages" value="Messages" />
-		<input type="button" name="notifications" value="Notifications" />
-		<span><?php echo $_SESSION['SESS_FIRST_NAME']." ".$_SESSION['SESS_LAST_NAME']; ?> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-		<a href="./logout.php">Logout</a></span>
-</div>
